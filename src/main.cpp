5595252d3fbbe677d590a39415f7d1ec252f1ad9
ivandmitrievsky <ivan.dmitrievsky@gmail.com>
2013-10-18 23:23:39 +0400

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  graphcut
+//
+//  Created by Ivan Dmitrievsky on 18.10.13.
+//  Copyright (c) 2013 Ivan Dmitrievsky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
