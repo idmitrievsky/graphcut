b80e903c88e9d63b3cf69f72f12004d190018852
ivandmitrievsky <ivan.dmitrievsky@gmail.com>
2013-11-09 01:34:21 +0400

    reimplemented graphs with adjacency list

--- src/graph.cpp
+++ src/graph.cpp
@@ -9,6 +9,7 @@
 #include <vector>
 #include <queue>
 #include <iostream>
+#include <algorithm>
 #include "graph.h"
 
 int Graph::nodes(void)
@@ -16,25 +17,59 @@ int Graph::nodes(void)
     return _nodes;
 }
 
-double Graph::arcs(int i, int j)
+double Graph::getArcWeight(int i, int j)
 {
-    return _arcs[i][j];
-}
-
-Graph::Graph(int nodesNum)
-{
-    _nodes = nodesNum;
+    NEIGHBOURLIST::iterator neigh;
+    int found = 0;
     
-    _arcs = new double*[_nodes];
-    
-    for (int i = 0; i < _nodes; i++)
+    for (neigh = _arcs[i].begin(); neigh != _arcs[i].end(); neigh++)
     {
-        _arcs[i] = new double[_nodes];
-        for (int j = 0; j < _nodes; j++)
+        if (neigh->nodeNumber == j)
         {
-            _arcs[i][j] = 0;
+            found = 1;
+            break;
         }
     }
+    
+    if (found)
+    {
+        return neigh->arcWeight;
+    }
+    
+    return 0;
+}
+
+void Graph::setArcWeight(int i, int j, double weight)
+{
+    NEIGHBOURLIST::iterator neigh;
+    int found = 0;
+    Neighbour newNeigh = {j, weight};
+    
+    for (neigh = _arcs[i].begin(); neigh != _arcs[i].end(); neigh++)
+    {
+        if (neigh->nodeNumber == j)
+        {
+            found = 1;
+            break;
+        }
+    }
+    
+    if (found)
+    {
+        neigh->arcWeight = weight;
+    }
+    else
+    {
+        _arcs[i].push_front(newNeigh);
+    }
+}
+
+Graph::Graph(int nodesNumber)
+{
+    _nodes = nodesNumber;
+    
+    _arcs = new NEIGHBOURLIST [_nodes];
+    
     print();
 }
 
@@ -42,15 +77,11 @@ Graph::Graph(const Graph&graph)
 {
     _nodes = graph._nodes;
     
-    _arcs = new double*[_nodes];
+    _arcs = new NEIGHBOURLIST [_nodes];
     
     for (int i = 0; i < _nodes; i++)
     {
-        _arcs[i] = new double[_nodes];
-        for (int j = 0; j < _nodes; j++)
-        {
-            _arcs[i][j] = graph._arcs[i][j];
-        }
+            _arcs[i] = graph._arcs[i];
     }
 }
 
@@ -60,15 +91,9 @@ Graph & Graph::operator = (const Graph &graph)
     {
         _nodes = graph._nodes;
         
-        _arcs = new double*[_nodes];
-        
         for (int i = 0; i < _nodes; i++)
         {
-            _arcs[i] = new double[_nodes];
-            for (int j = 0; j < _nodes; j++)
-            {
-                _arcs[i][j] = graph._arcs[i][j];
-            }
+            _arcs[i] = graph._arcs[i];
         }
     }
     
@@ -78,11 +103,6 @@ Graph & Graph::operator = (const Graph &graph)
 
 Graph::~Graph(void)
 {
-    for (int i = 0; i < _nodes; i++)
-    {
-        delete [] _arcs[i];
-    }
-    
     delete [] _arcs;
 }
 
@@ -90,36 +110,36 @@ void Graph::testFillUp(void)
 {
     int i = 0, j = 0;
     
-    _arcs[0][1] = 12;
-    _arcs[0][2] = 15;
-    _arcs[0][3] = 20;
-    _arcs[1][2] = 5;
-    _arcs[1][5] = 5;
-    _arcs[1][6] = 2;
-    _arcs[2][3] = 11;
-    _arcs[2][4] = 3;
-    _arcs[2][6] = 6;
-    _arcs[3][4] = 4;
-    _arcs[3][7] = 8;
-    _arcs[4][6] = 6;
-    _arcs[4][7] = 1;
-    _arcs[5][6] = 9;
-    _arcs[5][8] = 18;
-    _arcs[6][7] = 7;
-    _arcs[6][8] = 13;
-    _arcs[7][8] = 10;
+    setArcWeight(0, 1, 12);
+    setArcWeight(0, 2, 15);
+    setArcWeight(0, 3, 20);
+    setArcWeight(1, 2, 5);
+    setArcWeight(1, 5, 5);
+    setArcWeight(1, 6, 2);
+    setArcWeight(2, 3, 11);
+    setArcWeight(2, 4, 3);
+    setArcWeight(2, 6, 6);
+    setArcWeight(3, 4, 4);
+    setArcWeight(3, 7, 8);
+    setArcWeight(4, 6, 6);
+    setArcWeight(4, 7, 1);
+    setArcWeight(5, 6, 9);
+    setArcWeight(5, 8, 18);
+    setArcWeight(6, 7, 7);
+    setArcWeight(6, 8, 13);
+    setArcWeight(7, 8, 10);
     
     for (i = 0; i < _nodes; i++)
     {
         for (j = 0; j < i + 1; j++)
         {
-            if (_arcs[i][j])
+            if (getArcWeight(i, j))
             {
-                _arcs[j][i] = _arcs[i][j];
+                setArcWeight(j, i, getArcWeight(i, j));;
             }
-            if (_arcs[j][i])
+            if (getArcWeight(j, i))
             {
-                _arcs[i][j] = _arcs[j][i];
+                setArcWeight(i, j, getArcWeight(j, i));;
             }
         }
     }
@@ -127,39 +147,71 @@ void Graph::testFillUp(void)
 
 void Graph::erase(void)
 {
-    int i, j;
+    int i = 0;
     
     for (i = 0; i < _nodes; i++)
     {
-        for (j = 0; j < _nodes; j++)
-        {
-            _arcs[i][j] = 0;
-        }
+        _arcs[i].clear();
     }
 }
 
 void Graph::print(void)
 {
-    int i, j;
+    int i = 0;
+    NEIGHBOURLIST::iterator neigh;
     
     for (i = 0; i < _nodes; i++)
     {
-        for (j = 0; j < _nodes; j++)
+        for (neigh = _arcs[i].begin(); neigh != _arcs[i].end(); neigh++)
         {
-            std::cout << _arcs[i][j] << ' ';
+            std::cout << '(' << i << ", " << neigh->nodeNumber << ", " << neigh->arcWeight << ") ";
         }
         std::cout << std::endl;
     }
     std::cout << std::endl << std::endl;
 }
 
+void Graph::removeArc(int i, int j)
+{
+    NEIGHBOURLIST::iterator neigh, prev;
+    int found = 0;
+
+    if (_arcs[i].empty())
+    {
+        return;
+    }
+    
+    neigh = _arcs[i].begin();
+
+    if (neigh->nodeNumber == j)
+    {
+        _arcs[i].pop_front();
+        return;
+    }
+    
+    prev = neigh;
+    for (neigh++; neigh != _arcs[i].end(); neigh++)
+    {
+        if (neigh->nodeNumber == j)
+        {
+            found = 1;
+            break;
+        }
+        prev = neigh;
+    }
+    
+    if (found)
+    {
+        _arcs[i].erase_after(prev);
+    }
+}
 
 void Graph::bfs(void)
 {
     std::vector<int> visitedNodes(_nodes, 0);
     std::queue<int> toVisit;
-    int currentNode = 0,
-        neighbour   = 0;
+    int currentNode = 0;
+    NEIGHBOURLIST::iterator neigh;
     
     /* Start from the first node */
     visitedNodes[0] = 1;
@@ -170,18 +222,13 @@ void Graph::bfs(void)
         currentNode = toVisit.front();
         toVisit.pop();
         
-        for (int i = 0; i < _nodes; i++)
+        for (neigh = _arcs[currentNode].begin(); neigh != _arcs[currentNode].end(); neigh++)
         {
-            /* If <i> is unreacheable from <currentNode> */
-            if (_arcs[currentNode][i] <= 0)
+            /* If <i> is reacheable from <currentNode> */
+            if (!visitedNodes[neigh->nodeNumber])
             {
-                continue;
-            }
-            neighbour = i;
-            if (!visitedNodes[neighbour])
-            {
-                visitedNodes[neighbour] = 1;
-                toVisit.push(neighbour);
+                visitedNodes[neigh->nodeNumber] = 1;
+                toVisit.push(neigh->nodeNumber);
             }
         }
     }
