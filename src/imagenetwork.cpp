f7d9d7d008f610e5e5a85140e02abc290d861a2d
ivandmitrievsky <ivan.dmitrievsky@gmail.com>
2013-11-22 17:25:56 +0400

    fixed a bug in boundWeight
    - changed Scharr to Sobel in grad
    - provided options for terms

--- src/imagenetwork.cpp
+++ src/imagenetwork.cpp
@@ -14,7 +14,6 @@
 static cv::Mat *gradient(cv::Mat *image)
 {
     cv::Mat *grad;
-    cv::Mat angle;
     int ddepth = CV_16S;
     
     grad = new cv::Mat();
@@ -25,14 +24,22 @@ static cv::Mat *gradient(cv::Mat *image)
     cv::Mat abs_grad_x, abs_grad_y;
     
     /// Gradient X
-    cv::Scharr(*image, grad_x, ddepth, 1, 0);
-    //Sobel( src_gray, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
+    //cv::Scharr(*image, grad_x, ddepth, 1, 0);
+    cv::Sobel(*image, grad_x, ddepth, 1, 0, 3);
     cv::convertScaleAbs( grad_x, abs_grad_x );
     
     /// Gradient Y
-    cv::Scharr(*image, grad_y, ddepth, 0, 1);
-    //Sobel( src_gray, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
+    //cv::Scharr(*image, grad_y, ddepth, 0, 1);
+    cv::Sobel(*image, grad_y, ddepth, 0, 1, 3);
     cv::convertScaleAbs( grad_y, abs_grad_y );
+    
+    
+//    cv::Mat sq_grad;
+//    cv::Mat sq_grad_x, sq_grad_y;
+//    cv::multiply(abs_grad_x, abs_grad_x, sq_grad_x);
+//    cv::multiply(abs_grad_y, abs_grad_y, sq_grad_y);
+//    cv::add(sq_grad_x, sq_grad_y, sq_grad);
+//    cv::sqrt(sq_grad, *grad);
     
     /// Total Gradient (approximate)
     cv::addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, *grad);
@@ -50,6 +57,8 @@ ImageNetwork::ImageNetwork(const char *imagePath)
     
     *image = cv::imread(imagePath, CV_LOAD_IMAGE_GRAYSCALE);
     grad = gradient(image);
+    cv::imwrite("/Users/ivan/.supp/code/graphcut/grad.jpg", *grad);
+    
     pixels = image->rows * image->cols;
     
     nodesNumber = pixels + 2;
@@ -150,39 +159,113 @@ void ImageNetwork::pixelNeighbours(graphIndex p, std::vector<graphIndex> &neighb
 
 double ImageNetwork::boundaryTerm(graphIndex p, Label pLabel, graphIndex q, Label qLabel)
 {
-    double sigma = 30, distX = 0, distY = 0, distance = 0, weight = 0, intensDiff = 0;
+    /* 1 */
     
-    distX = fabs(nodeX(p) - nodeX(q));
-    distY = fabs(nodeY(p) - nodeY(q));
+    double weight = 0, eps = 1.0, kapp = 1, alpha = 100;
+    double temp, tempo;
     
-    distance = sqrt(distX * distX + distY * distY);
-    intensDiff = nodeIntensity(p) - nodeIntensity(q);
+    if (pLabel == qLabel)
+    {
+        return weight = 0;
+    }
+    else
+    {
+        temp = abs(nodeIntensity(p) - nodeIntensity(q));
+        temp = temp * temp;
+        tempo = abs(nodeIntensity(p) + nodeIntensity(q));
+        tempo = tempo * tempo + eps;
+        weight = alpha * exp(- kapp * (temp / tempo));
+        return weight;
+    }
     
-    weight = exp(-(intensDiff * intensDiff) / (2 * sigma * sigma));
-    weight /= distance;
+    /* 2 */
     
-    return weight;
-    //return 80 * fabs(label(p) - label(q));
+//    double sigma = 30, distX = 0, distY = 0, distance = 0, weight = 0, intensDiff = 0;
+//    
+//    distX = fabs(nodeX(p) - nodeX(q));
+//    distY = fabs(nodeY(p) - nodeY(q));
+//    
+//    distance = sqrt(distX * distX + distY * distY);
+//    intensDiff = nodeIntensity(p) - nodeIntensity(q);
+//    
+//    weight = exp(-(intensDiff * intensDiff) / (2 * sigma * sigma));
+//    weight /= distance;
+//    
+//    return weight;
+    
+    /* 3 */
+    
+//    return 80 * fabs(label(p) - label(q));
 }
 
 double ImageNetwork::localTerm(graphIndex p, Label pLabel)
 {
-    double weight = 0;
+    /* 1 */
     
-    switch (pLabel)
+//    double weight = 0;
+//    std::vector<graphIndex> neighbours(4);
+//    std::vector<graphIndex>::iterator it;
+//    int i = 0, hist = 1;
+//    
+//    pixelNeighbours(i, neighbours);
+//    
+//    for (it = neighbours.begin(); it != neighbours.end(); it++)
+//    {
+//        hist += (partition->label(*it - 1) != pLabel ? 1 : 0);
+//    }
+//    
+//    weight = 10 * log(hist);
+//    return weight;
+    
+    /* 2 */
+    
+//    double weight = 0;
+//    
+//    switch (pLabel)
+//    {
+//        case FOREGROUND:
+//            weight = - fmax(0, grad->at<uchar>(nodeX(p), nodeY(p)));
+//            break;
+//        case BACKGROUND:
+//            weight = fmax(0, grad->at<uchar>(nodeX(p), nodeY(p)));
+//            break;
+//        default:
+//            break;
+//    }
+//    
+//    return weight;
+    
+    /* 3 */
+    
+//    return fmin(fabs(label - intencity), intencity);
+    /* 4 */
+    
+    if (nodeIntensity(p) < 50 && pLabel == BACKGROUND)
     {
-        case FOREGROUND:
-            weight = - fmin(0, grad->at<uchar>(nodeX(p), nodeY(p)));
-            break;
-        case BACKGROUND:
-            weight = fmax(0, grad->at<uchar>(nodeX(p), nodeY(p)));
-            break;
-        default:
-            break;
+        return 2000000;
     }
-    
-    return weight;
-    //return fmin(fabs(label - intencity), intencity);
+    else if (nodeIntensity(p) > 100 && pLabel == FOREGROUND)
+    {
+        return 2000000;
+    }
+    else
+    {
+        double weight = 0;
+        std::vector<graphIndex> neighbours(4);
+        std::vector<graphIndex>::iterator it;
+        int i = 0, hist = 1;
+        
+        pixelNeighbours(i, neighbours);
+        
+        for (it = neighbours.begin(); it != neighbours.end(); it++)
+        {
+            hist += (partition->label(*it - 1) != pLabel ? 1 : 0);
+        }
+        
+        weight = 10 * log(hist);
+        return weight;
+
+    }
 }
 
 
@@ -227,7 +310,7 @@ int ImageNetwork::repart(void)
         
         for (it = neighbours.begin(); it != neighbours.end(); it++)
         {
-            boundWeight = boundaryTerm(i, partition->label(i - 1), *it, partition->label(*it - 1));
+            boundWeight = boundaryTerm(i, FOREGROUND, *it, BACKGROUND);
             assoc.setArcWeight(*it, i, boundWeight);
             assoc.setArcWeight(i, *it, boundWeight);
         }
