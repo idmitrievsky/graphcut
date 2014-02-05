9cb5bf79fe247893917c9124a8b80d40f79cc52a
ivandmitrievsky <ivan.dmitrievsky@gmail.com>
2014-02-05 19:00:32 +0400

    changed main properly

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,41 @@
 #include <iostream>
 #include "imagenetwork.h"
 
+#define UNITPX 32
+
 int main(int argc, const char * argv[])
 {
-    ImageNetwork image(argv[1]);
+    cv::Mat image, imageROI, out, outROI;
+    unsigned vn = 0, hn = 0;
     
-    while (image.repart());
+    image = cv::imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+    out = image;
     
-    image.outputPartition(argv[2]);
+    vn = image.rows / UNITPX;
+    hn = image.cols / UNITPX;
+    
+    for (unsigned x = 0; x < hn; ++x)
+    {
+        for (unsigned y = 0; y < vn; ++y)
+        {
+            outROI = imageROI = image(cv::Rect(x * UNITPX, y * UNITPX, UNITPX, UNITPX));
+            
+            ImageNetwork temp(&imageROI);
+            while (temp.repart());
+            
+            temp.outputPartition(&outROI);
+            
+            for (unsigned in = 0; in < UNITPX; ++in)
+            {
+                for (unsigned jn = 0; jn < UNITPX; ++jn)
+                {
+                    out.at<uchar>(y * UNITPX + jn, x * UNITPX + in) = outROI.at<uchar>(jn, in);
+                }
+            }
+        }
+    }
+    
+    cv::imwrite(argv[2], out);
     
     return 0;
 }
